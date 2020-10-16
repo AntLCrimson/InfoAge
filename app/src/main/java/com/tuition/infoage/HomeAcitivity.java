249942f1e4fbd9707227bacd634bf29bd141919c
iminb2b <65203827+iminb2b@users.noreply.github.com>
2020-10-15 22:16:50 -0400

    Final Version for milestone 1

--- app/src/main/java/com/tuition/infoage/HomeAcitivity.java
+++ app/src/main/java/com/tuition/infoage/HomeAcitivity.java
@@ -1,10 +1,13 @@
 package com.tuition.infoage;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 
 public class Main_board extends AppCompatActivity {
@@ -13,16 +16,6 @@ public class Main_board extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_board);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
