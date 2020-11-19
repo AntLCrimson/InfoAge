c082d390c0240c0d172333b9c3a0efaa7922d6ed
iminb2b <65203827+iminb2b@users.noreply.github.com>
2020-11-18 23:03:33 -0500

    Add activities

--- app/src/main/java/com/tuition/infoage/Class1Activity.java
+++ app/src/main/java/com/tuition/infoage/Class1Activity.java
@@ -9,6 +9,6 @@ public class Class1Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_class1);
+        setContentView(R.layout.connection_lost);
     }
 }
